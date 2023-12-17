36037aadcd2bdc7154491d4a2bfa0cbb70976ed6
Subhadip Das <subhadipdas911@gmail.com>
2023-12-17 14:01:20 +0530

    Wrote async function to make network request to fetch the Categories and also printed the data to cross verify

--- Ecommerce Website/Main Folder/js/index.js
+++ Ecommerce Website/Main Folder/js/index.js
@@ -1,1 +1,9 @@
-console.log("Inside the JS File");
+async function fetchCategories(){
+      let response = await fetch("https://fakestoreapi.com/products/categories");
+      let data = await response.json();
+      return data;
+};
+
+let categoryData = fetchCategories();  
+
+categoryData.then((data) => {console.log(data)});
