321575c7a9ac147c5486da440c5a9ed2b02711b6
Subhadip Das <subhadipdas911@gmail.com>
2023-12-17 15:07:13 +0530

    Implemented the function populateCategories() which populated the response from the server to the UI. Also commented out a few instructions for better understanding of the FLOW

--- Ecommerce Website/Main Folder/js/index.js
+++ Ecommerce Website/Main Folder/js/index.js
@@ -4,6 +4,28 @@ async function fetchCategories(){
       return data;
 };
 
-let categoryData = fetchCategories();  
+// Using fetchCategories() I am fetching the data
+// Using populateCategories() I am displaying all the categories in the UI.
 
-categoryData.then((data) => {console.log(data)});
+async function populateCategories(){
+      let categories = await fetchCategories();
+      let categoryListDiv = document.querySelector("#category-list");
+
+      for (let i=0; i<categories.length; i++){
+
+            let currentCategoryHolder = document.createElement("div");
+            currentCategoryHolder.classList.add("category-item","d-flex","flex-column","justify-content-center");
+
+            let currentCategoryLink = document.createElement("a");
+
+            currentCategoryLink.textContent = categories[i]; // Here we are setting the incoming category Name inside the Anchor Tag
+
+            currentCategoryLink.href = "#";
+
+            currentCategoryHolder.appendChild(currentCategoryLink);
+
+            categoryListDiv.appendChild(currentCategoryHolder);
+      }
+}
+
+populateCategories();
