d99f809672c9e810526eacb663f921466918cc75
Subhadip Das <subhadipdas911@gmail.com>
2023-12-18 17:16:30 +0530

    Implemented Filter Functionality and also added relevant comments to explain the Flow

--- Ecommerce Website/Main Folder/js/productList.js
+++ Ecommerce Website/Main Folder/js/productList.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded",function(e){
             let data = await response.json();
             return data;
       }
+
+      function populateDOM(array){ // This function is used to populate the DOM
       
-      async function populateProducts(){
-            let productsList = await fetchProducts();
             let productListBox = document.querySelector("#product-list");
       
-            for (let i=0; i<productsList.length; i++){
+            for (let i=0; i<array.length; i++){
       
                   let productLink = document.createElement("a");
                   productLink.href = "productDetails.html"; // As of now we will be able to redirect the same product detail.
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded",function(e){
                   productImageHolder.classList.add("product-img");
             
                   let productImage = document.createElement("img");
-                  productImage.src = productsList[i].image; // Loaded the Product Image
+                  productImage.src = array[i].image; // Loaded the Product Image
       
                   productImageHolder.appendChild(productImage);
       
@@ -30,22 +30,56 @@ document.addEventListener("DOMContentLoaded",function(e){
                   let productName = document.createElement("div");
                   productName.classList.add("product-name","text-center");
       
-                  productName.textContent = productsList[i].title.substring(0,12)+"..."; // Populating the Title of the Product.
+                  productName.textContent = array[i].title.substring(0,12)+"..."; // Populating the Title of the Product.
       
                   productLink.appendChild(productName);
       
                   let productPrice = document.createElement("div");
                   productPrice.classList.add("product-price","text-center");
       
-                  productPrice.textContent = `\u20B9 ${productsList[i].price}`; // Populating the Price of the Product
+                  productPrice.textContent = `\u20B9 ${array[i].price}`; // Populating the Price of the Product
       
                   productLink.appendChild(productPrice);
       
                   productListBox.appendChild(productLink); //Appending the newly created product to the Product List Box
             };
+      }
+      
+      async function populateProducts(){
+            let productsList = await fetchProducts();
+            populateDOM(productsList)
       
       };
       
       populateProducts();
-})
+
+      let minPrice = document.querySelector("#minPrice");
+
+      let maxPrice = document.querySelector("#maxPrice");
+
+      let searchBtn = document.querySelector("#search");
+
+      searchBtn.addEventListener("click",async function(e){
+            let minBoundary = minPrice.value;
+            let maxBoundary = maxPrice.value;
+            let productList = await fetchProducts();
+
+            let productListBox = document.querySelector("#product-list"); // This is the parent under which all the filtered records will come
+
+            productListBox.innerHTML = ""; // Every time we use new filter parameters, we will remove the old products from the DOM and populate the DOM with newly filtered array.
+
+            let filteredProducts = productList.filter(function(value){
+                  if (value.price >= minBoundary && value.price <= maxBoundary){
+                        return true;
+                  }else{
+                        return false;
+                  }
+            });
+
+            // Now that we have our Filtered Products we can call populateDOM and pass in the filtered array to populate our DOM
+            
+            populateDOM(filteredProducts);
+      });
+
+});
 
